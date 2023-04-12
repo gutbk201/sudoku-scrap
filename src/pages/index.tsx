f1e1cac653350823f34f67a06cd3f4bb783a7a2a
gutbk201 <siuym4mym4@gmail.com>
2023-04-12 06:27:56 +0000

    better css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-import { Difficulty } from '~/utils/constant';
+import { Difficulty, RawDifficulty } from '~/utils/constant';
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { type NextPage } from "next";
-import { useState } from 'react';
 import { IRawDifficulty } from '~/utils/types';
-
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { ReactNode } from "react";
+export function objectEntries<
+  T extends Record<PropertyKey, unknown>,
+  K extends keyof T,
+  V extends T[K]
+>(o: T) {
+  return Object.entries(o) as [K, V][];
+}
+type Iinputs = {
+  // type: string,
+  times: number,
+  diff: IRawDifficulty
+};
 export function downloadBase64(name: string, url: string) {
   const link = document.createElement("a");
   link.href = url;
@@ -13,7 +25,17 @@ export function downloadBase64(name: string, url: string) {
   link.click();
 }
 const Home: NextPage = () => {
-  const [fields, setFields] = useState({ times: 12 });
+  const { register, handleSubmit, control, formState: { errors } } = useForm({
+    defaultValues: {
+      diff: Difficulty.normal,
+      times: 12,
+    }
+  });
+  const onGrab: SubmitHandler<Iinputs> = data => {
+    if (apis.grabSudoku.isLoading) return;
+    const params = { type: 'classic', ...data };
+    apis.grabSudoku.mutate(params);
+  };
   const countApi = async () => {
     const res = await fetch('api/sudoku')
     return res.json()
@@ -21,7 +43,6 @@ const Home: NextPage = () => {
   const countRes = useQuery(['sudoku'], countApi)
   const apis = useApis(
     {
-      params: { type: 'classic', times: fields.times, diff: Difficulty.normal },
       onDownloadSuccess: (data) => {
         const { base64, fileName } = data || {};
         downloadBase64(fileName, base64)
@@ -32,44 +53,53 @@ const Home: NextPage = () => {
       }
     },
   );
-  const onGrab = () => {
-    if (apis.grabSudoku.isLoading) return;
-    apis.grabSudoku.mutate();
-  }
   const onDownload = () => {
     apis.download.mutate();
   }
   const imageUrl = apis.download.data?.base64
   const enableBtnGrab = apis.grabSudoku.isIdle || apis.grabSudoku.isSuccess;
+  function LabelInput(p: { label: string, input: ReactNode }) {
+    return (<div className="flex justify-between">
+      <label className="flex-1 max-w-2/5 overflow-auto">{p.label}</label>
+      <span className="flex-1 max-w-2/5 overflow-auto">{p.input}</span>
+    </div>)
+  }
   return (
     <main className="m-2">
       <div>Sudoku Graber</div>
-      <input name="times" type="number" min="6" step="6" value={fields.times} onChange={e => setFields(f => ({ ...f, times: Number(e.target.value) }))} />
-      <button onClick={onGrab} className={`opacity-${enableBtnGrab ? 100 : 50} p-2 m-2 border-2 border-black`}>Grab</button>
-      <button onClick={onDownload} className="p-2 m-2 border-2 border-black">Download</button>
-      {apis.grabSudoku.isLoading && <div>Loading...</div>}
-      <div>Count: {countRes.data?.count || ""}</div>
+      <form onSubmit={handleSubmit(onGrab)} className='flex flex-col w-80 gap-y-2	p-4 border-solid border-2'>
+        <LabelInput
+          label="Times"
+          input={<input type="number" step="6" min="6" {...register("times", { required: true })}
+            className="w-full" />}
+        />
+        <LabelInput
+          label="Difficulty"
+          input={<select {...register("diff")} className="w-full">
+            {objectEntries(RawDifficulty).map(([diff, value]) => (<option key={value} value={value} >{diff}</option>))}
+          </select>}
+        />
+        {apis.grabSudoku.isLoading && <div>Loading...</div>}
+        <div>Count: {countRes.data?.count || ""}</div>
+        <input type="submit" className={`p-2 m-2 border-2 border-black opacity-${enableBtnGrab ? 100 : 50}`} value="Grab" />
+        <button onClick={onDownload} className="p-2 m-2 border-2 border-black">Download</button>
+      </form>
       <div className="bg-slate-200">
         {imageUrl && <img src={imageUrl} />}
       </div>
     </main>
   );
 };
+
 function useApis(p:
   {
-    params: { times: number, diff: IRawDifficulty, type: 'classic' }
     onDownloadSuccess: ((data: any, variables: void, context: unknown) => unknown) | undefined,
     onGrabSuccess: ((data: any, variables: void, context: unknown) => unknown) | undefined,
   }
 ) {
-  const { params } = p;
   const apis = {
-    // grabSudoku: useMutation({
-    //   mutationFn: () => fetch('api/grabSudokus').then(r => r.json()),
-    //   onSuccess: p.onGrabSuccess
-    // }),
-    grabSudoku: useMutation({
-      mutationFn: () => axios.post('api/sudoku', { type: params.type, times: params.times, diff: params.diff }),
+    grabSudoku: useMutation<any, any, any>({
+      mutationFn: (params: { times: number, diff: IRawDifficulty, type: 'classic' }) => axios.post('api/sudoku', params),
       onSuccess: p.onGrabSuccess
     }),
     download: useMutation({
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {maxWidth: {
+      '1/2': '50%',
+      '1/3': '33%',
+      '2/5': '40%',
+    }},
   },
   plugins: [],
 };
